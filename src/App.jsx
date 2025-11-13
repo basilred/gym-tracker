c0c77ee485389bf6e4d901edef8bffa839a8a9fa
Vasily Krasnov <mykrasnov@gmail.com>
2025-11-13 20:39:36 +0300

    Add gh pages' base to the Vite config

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SubscriptionPage from "./pages/SubscriptionPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/gym-tracker/'}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/subscription/:id" element={<SubscriptionPage />} />
