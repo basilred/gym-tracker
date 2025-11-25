5e495ebd9c9385e3f184ece7d4a1eedec1bb3fc0
Vasily Krasnov <mykrasnov@gmail.com>
2025-11-25 18:00:39 +0300

    feat: Add PWA support with manifest, icon, and `vite-plugin-pwa` configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+        manifest: {
+          name: 'Gym Tracker',
+          short_name: 'Gym Tracker',
+          description: 'A simple app to track your gym subscriptions',
+          theme_color: '#fff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        }
+      }),
+    ],
     base: mode === 'development' ? '/' : '/gym-tracker/',
   }
 })
